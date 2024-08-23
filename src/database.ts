e420fafe063208fb924ffa1a019879ae638724bf
SoRaang <dsc0320@hotmail.com>
2024-08-23 17:13:08 +0900

    Mid-save

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { User } from './types';
+
+const user: User[] = [
+    { id: 1, name: '김창완', address: '부산', email: 'dsc0320@hotmail.com', description: '미남' },
+    { id: 2, name: '김창완', address: '부산', email: 'dsc0320@hotmail.com', description: '미남' },
+    { id: 3, name: '김창완', address: '부산', email: 'dsc0320@hotmail.com', description: '미남' },
+    { id: 4, name: '김창완', address: '부산', email: 'dsc0320@hotmail.com', description: '미남' },
+    { id: 5, name: '김창완', address: '부산', email: 'dsc0320@hotmail.com', description: '미남' }
+]
+
+const dataBase = {
+    user
+}
+
+export default dataBase;
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8001;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen({port: PORT}, () => {
+    console.log(`서버 실행 완료. http://localhost:${ PORT }`);
+});
+
+export default app;
